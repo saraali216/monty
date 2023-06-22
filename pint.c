d296a29a8487fb6c74e80ac94226e38e340e7958
saraali <saraali.no3man@gmail.com>
2023-06-22 15:28:14 -0700

    pint and swap

---
+++ pint.c
@@ -0,0 +1,61 @@
+#include "monty.h"
+
+/**
+ * pint .
+ * @stack: Pointer to the top of the stack
+ * @ln: Line number of the current operation
+ */
+
+void pint(stack_t **stack, unsigned int ln)
+{
+	if (stack == NULL || *stack == NULL)
+		print_pint_error(ln);
+
+	printf("%d\n", (*stack)->n);
+}
+/**
+ * ppint .
+ * @ln: Line number of the current operation
+ */
+void ppint(int ln)
+{
+
+	fprintf(stderr, "L%d: can't pint, stack empty\n", ln);
+	free_everything();
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * swap .
+ * @stack: Pointer to the top of the stack
+ * @ln: Line number of the current operation
+ */
+
+void swap(stack_t **stack, unsigned int ln)
+{
+	int temp;
+
+	if (stack == NULL || *stack == NULL)
+		pswap(ln);
+
+	if ((*stack)->next)
+	{
+		temp = (*stack)->n;
+		(*stack)->n = (*stack)->next->n;
+		(*stack)->next->n = temp;
+	}
+	else
+		pswap(ln);
+}
+/**
+ * pswap .
+ * @ln: Line number of the current operation
+ */
+
+void pswap(int ln)
+{
+
+	fprintf(stderr, "L%d: can't swap, stack too short\n", ln);
+	free_everything();
+	exit(EXIT_FAILURE);
+}
--- pop_nop.c
+++ pop_nop.c
@@ -1,5 +1,11 @@
 #include "monty.h"
 
+/**
+ * pop .
+ * @stack: Pointer to the top of the stack
+ * @ln: Line number of the current operation
+ */
+
 void pop(stack_t **stack, unsigned int ln)
 {
 	if(stack == NULL || *stack == NULL)
@@ -8,13 +14,24 @@ void pop(stack_t **stack, unsigned int ln)
 	delete_top_node(stack);
 }
 
+/**
+ * nop .
+ * @stack: Pointer to the top of the stack
+ * @ln: Line number of the current operation
+ */
+
 void nop(stack_t **stack, unsigned int ln)
 {
 	UNUSED(stack);
 	UNUSED(ln);
 }
 
-void print_empty_stack_error(int ln)
+/**
+ * pempty_stack .
+ * @ln: Line number of the current operation
+ */
+
+void pempty_stack(int ln)
 {
 	fprintf(stderr, "L%d: can't pop an empty stack\n", ln);
 	free_everything();
