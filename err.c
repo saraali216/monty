26b731f0df7542a5001ccf810c9e14033d9c0c63
Ichrafsassi <ichraf.sassi.456@gmail.com>
2023-06-22 17:11:34 -0700

    err.c

--- err.c
+++ err.c
@@ -24,4 +24,21 @@ void pinvalid_instruction(int ln, char *opcode_instruction)
 	exit(EXIT_FAILURE);
 }
 
+/**
+ * pusage -prints the usage error file
+ */
+void pusage(void)
+{
+	fprintf(stderr, "USAGE: the monty file :\n");
+	exit(EXIT_FAILURE);
+}
 
+/**
+ * open_file_error-prints the file name has the error
+ * @fn: file name
+ */
+void open_file_error(char *fn)
+{
+	fprintf(stderr, "Error: Can't open the file %s\n", fn);
+	exit(EXIT_FAILURE);
+}
