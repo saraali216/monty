64bde8d110249820d30b7131b45a75efed6916ba
Ichrafsassi <ichraf.sassi.456@gmail.com>
2023-06-22 17:07:03 -0700

    err.c

--- err.c
+++ err.c
@@ -10,4 +10,18 @@ void pmalloc(void)
 	exit(EXIT_FAILURE);
 }
 
+/**
+ * pinvalid_instruction-Prints an error message and exits when an
+ * unrecognized opcode is encountered
+ * @ln: current operation's line number
+ * @opcode_instruction: points to unrecognized opcode
+ */
+void pinvalid_instruction(int ln, char *opcode_instruction)
+{
+
+	fprintf(stderr, "L%d: unknown instruction %s\n", ln, opcode_instruction);
+	free_all();
+	exit(EXIT_FAILURE);
+}
+
 
