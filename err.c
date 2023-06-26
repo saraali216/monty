2b3990a2026237069c821fbc2c1e68fef055b08c
Ichrafsassi <50300666+Ichrafsassi@users.noreply.github.com>
2023-06-26 02:10:25 +0100

    Update err.c

--- err.c
+++ err.c
@@ -13,13 +13,13 @@ void pmalloc(void)
 /**
  * pinvalid_instruction-Prints an error message and exits when an
  * unrecognized opcode is encountered
- * @ln: current operation's line number
+ * @line_number: current operation's line number
  * @opcode_instruction: points to unrecognized opcode
  */
-void pinvalid_instruction(int ln, char *opcode_instruction)
+void pinvalid_instruction(int line_number, char *opcode_instruction)
 {
 
-	fprintf(stderr, "L%d: unknown instruction %s\n", ln, opcode_instruction);
+	fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode_instruction);
 	free_all();
 	exit(EXIT_FAILURE);
 }
